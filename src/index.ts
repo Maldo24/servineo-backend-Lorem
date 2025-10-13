bdffb934d109f2ec4185a8c032fb18caf06b5803
Leonel Zeballos Aldunate <zeballos.leonel@outlook.com>
2025-10-13 17:11:29 -0400

    export app for vercel

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,11 @@
-import Server from './config/server.config';
+import app from './config/server.config';
 import _connect from './database';
-
 import { SERVER_PORT } from './config/env.config';
 
-async function startServer() {
-  try {
-    Server.listen(SERVER_PORT, () => {
-      console.info(`Server running on http://localhost:${SERVER_PORT}`);
-    });
-  } catch (error) {
-    console.error('Error starting server', error);
-  }
-}
-
 _connect();
-startServer();
+
+if (process.env.VERCEL === undefined) {
+  app.listen(SERVER_PORT, () => {
+    console.info(`Server running on http://localhost:${SERVER_PORT}`);
+  });
+}
