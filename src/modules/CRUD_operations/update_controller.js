93ef99a15c1f179cdeb0b0ffa3b3d2e92667e4e2
Anahí <anahi.sanabria@studentambassadors.com>
2025-10-14 17:09:22 -0400

    fixed

--- src/modules/CRUD_operations/update_controller.js
+++ src/modules/CRUD_operations/update_controller.js
@@ -132,7 +132,7 @@ export async function updateManyLocationsFieldsByQuery(res, req) {
   }
 }
 
-// Appointments - Controladores faltantes
+// Appointments
 export async function updateAppointmentById(req, res) {
   try {
     const id = req.query.id;
